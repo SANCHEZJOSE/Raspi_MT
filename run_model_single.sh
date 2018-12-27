61cd853eb08a9afd26128ca17aa98e5b036af749
SANCHEZJOSE <jose.san.ala@gmail.com>
2018-12-27 19:18:05 -0300

    implementacion de timeout

--- run_model_single.sh
+++ run_model_single.sh
@@ -5,7 +5,7 @@ rm -vf prog.o
 g++ -Wall -o prog.o mediciones_single.cpp ADS1115.cpp ADS1256.cpp ini/ini.c ini/cpp/INIReader.cpp -lbcm2835
 echo "Modelo compilado"
 # Paso 2, Generar la fecha para el nombre de datos.txt
-fecha=`date +%Y%m%d_%H:%M:%S`
+fecha=`date +%Y%m%d_%H%M%S`
 echo $fecha
 # Paso 3, Verificar la existencia del archivo
 archivo='datos_'${fecha}'.txt'
