8698302bfc717c769eba07e17daea10a3fb0bd6c
SANCHEZJOSE <jose.san.ala@gmail.com>
2018-12-27 19:23:25 -0300

    implementacion de timeout

--- run_model.sh
+++ run_model.sh
@@ -5,7 +5,7 @@ rm -v prog.o
 g++ --std=c++0x -Wall -o prog.o mediciones.cpp ADS1115.cpp ADS1256.cpp ini/ini.c ini/cpp/INIReader.cpp -lbcm2835
 echo "Modelo compilado"
 # Paso 2, generar el nombre de datos.txt
-fecha=`date +%Y%m%d_%H:%M:%S`
+fecha=`date +%Y%m%d_%H%M%S`
 echo $fecha
 # Paso 3, corre el codigo
 sudo ./prog.o datos${fecha}.txt
